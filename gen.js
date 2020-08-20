cd5d1c5bdd7eba1481bea6280d08b50e8ae26ba2
TyGillespie <60036684+TyGillespie@users.noreply.github.com>
2020-08-20 11:38:01 -0600

    Add files via upload
    Fixed a small typo in the JavaScript binding and added the PHP version.

--- gen.js
+++ gen.js
@@ -5,7 +5,7 @@ function randomInt(min, max) {
 function generate(length = 8) {
     let symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
     let final = "";
-    for (let i = 1; i < length; i += 1) {
+    for (let i = 0; i < length; i += 1) {
         final += symbols[randomInt(0, symbols.length - 1)]
     }
     return final;
