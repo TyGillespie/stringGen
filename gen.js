b972f00c2adaa3ba085b2585ef7a0d716459ef46
TyGillespie <60036684+TyGillespie@users.noreply.github.com>
2020-09-09 07:33:51 -0600

    Added some examples and reformats
    Added an HTML (JavaScript) and C sharp example, as well as made the styling for all the code files consistent, and other random things, such as making a minified JS version.

--- gen.js
+++ gen.js
@@ -1,12 +1,12 @@
 "use strict";
 function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 function generate(length = 8) {
-    let symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-    let final = "";
-    for (let i = 0; i < length; i += 1) {
-        final += symbols[randomInt(0, symbols.length - 1)]
-    }
-    return final;
+	let symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+	let final = "";
+	for (let i = 0; i < length; i += 1) {
+		final += symbols[randomInt(0, symbols.length - 1)]
+	}
+	return final;
 }
--- gen.py
+++ gen.py
@@ -1,9 +1,7 @@
 import random
-
-
 def generate(length = 8):
-    symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-    final = ""
-    for i in range(length):
-        final += symbols[random.randint(0, len(symbols) - 1)]
-    return final
+	symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+	final = ""
+	for i in range(length):
+		final += symbols[random.randint(0, len(symbols) - 1)]
+	return final
