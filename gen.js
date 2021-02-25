7719fd6333cd97afdad0d8391739e1a7f679efa6
Ty <60036684+TyGillespie@users.noreply.github.com>
2021-02-24 21:19:54 -0700

    Way updated formatting.

--- gen.js
+++ gen.js
@@ -1,11 +1,13 @@
 "use strict";
+
 function randomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function generate(length = 8) {
+
+function generate(length=8) {
 	let symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 	let final = "";
-	for (let i = 0; i < length; i += 1) {
+	for (let i = 0; i < length; i++) {
 		final += symbols[randomInt(0, symbols.length - 1)]
 	}
 	return final;
--- gen.php
+++ gen.php
@@ -1,13 +1,13 @@
 <?php
-    function generate($length) {
-        $token = "";
-        $symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        $symbols .= "abcdefghijklmnopqrstuvwxyz";
-        $symbols .= "0123456789";
-        $max = strlen($symbols) - 1;
-        for ($i = 0; $i < $length; $i += 1) {
-            $token .= $symbols[rand(0, $max)];
-        }
-        return $token;
-    }
+	function generate($length) {
+		$token = "";
+		$symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		$symbols .= "abcdefghijklmnopqrstuvwxyz";
+		$symbols .= "0123456789";
+		$max = strlen($symbols) - 1;
+		for ($i = 0; $i < $length; $i++) {
+			$token .= $symbols[rand(0, $max)];
+		}
+		return $token;
+	}
 ?>
